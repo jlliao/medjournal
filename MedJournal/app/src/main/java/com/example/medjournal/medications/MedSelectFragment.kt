ddda3bff607b815c6bbe21312c8072d5fc36f4c6
StinsonOvO <tingsheng.shi@gmail.com>
2019-10-28 23:48:19 +0800

    working med select

--- MedJournal/app/src/main/java/com/example/medjournal/medications/MedSelectFragment.kt
+++ MedJournal/app/src/main/java/com/example/medjournal/medications/MedSelectFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,6 +16,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 import com.example.medjournal.R
 import com.example.medjournal.adapters.RecylerAdapter
+import com.google.android.material.textfield.TextInputEditText
 import java.util.*
 import java.util.Arrays.asList
 import kotlin.collections.ArrayList
@@ -25,6 +27,26 @@ import kotlin.collections.ArrayList
 class MedSelectFragment : Fragment() {
 
     private lateinit var linearLayoutManager: LinearLayoutManager
+
+    interface OnItemClickListener {
+        fun onItemClicked(position: Int, view: View)
+    }
+
+    private fun RecyclerView.addOnItemClickListener(onClickListener: OnItemClickListener) {
+        this.addOnChildAttachStateChangeListener(object: RecyclerView.OnChildAttachStateChangeListener {
+
+            override fun onChildViewDetachedFromWindow(view: View) {
+                view.setOnClickListener(null)
+            }
+
+            override fun onChildViewAttachedToWindow(view: View) {
+                view.setOnClickListener {
+                    val holder = getChildViewHolder(view)
+                    onClickListener.onItemClicked(holder.adapterPosition, view)
+                }
+            }
+        })
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,16 +60,21 @@ class MedSelectFragment : Fragment() {
             medSelectView.findNavController().navigate(R.id.action_medSelectFragment_to_medConfigFragment)
         }
 
-        val medications = ArrayList<String>(asList(*resources.getStringArray(R.array.medicine_names)))
+        val medications = ArrayList<String>(listOf(*resources.getStringArray(R.array.medicine_names)))
 
         val recyclerView = medSelectView.findViewById<RecyclerView>(R.id.rv_med_name)
         linearLayoutManager = LinearLayoutManager(activity)
         recyclerView.layoutManager = linearLayoutManager
         recyclerView.adapter = RecylerAdapter(medications, context!!)
         recyclerView.layoutParams.height = 800
-        recyclerView.setOnClickListener {
-            Log.d("Tests", "Clicked on recycler view")
-        }
+        recyclerView.addOnItemClickListener(object: OnItemClickListener {
+            override fun onItemClicked(position: Int, view: View) {
+                //Toast.makeText(activity, medications[position], Toast.LENGTH_SHORT).show()
+                medSelectView.findViewById<TextInputEditText>(R.id.et_med_name)
+                    .setText(medications[position])
+            }
+        })
+
         return medSelectView
     }
 
