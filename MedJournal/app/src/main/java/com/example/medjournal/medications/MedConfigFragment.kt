997439d9da82d2a7f39f5514ee9250b185d8e70d
Jianglong Liao <mike.liaojl@gmail.com>
2019-10-31 13:24:28 +0800

    update spinner

--- MedJournal/app/src/main/java/com/example/medjournal/medications/MedConfigFragment.kt
+++ MedJournal/app/src/main/java/com/example/medjournal/medications/MedConfigFragment.kt
@@ -6,8 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 import com.example.medjournal.R
+import kotlinx.android.synthetic.main.fragment_med_config.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,7 +23,18 @@ class MedConfigFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_med_config, container, false)
+
+        val adapter = ArrayAdapter.createFromResource(requireContext(), R.array.medication_frequency, android.R.layout.simple_spinner_item)
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        val medConfigView = inflater.inflate(R.layout.fragment_med_config, container, false)
+
+        val spinner = medConfigView.findViewById<Spinner>(R.id.medication_reminder_time_spinner)
+
+        spinner.adapter = adapter
+
+        return medConfigView
     }
 
 
